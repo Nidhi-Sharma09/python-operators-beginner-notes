cadcd3eea3607fdf38dea4f9be5f55d62f5ecdbb
Nidhi-Sharma09 <nidhi12sharma09@gmail.com>
2026-06-22 17:15:04 +0530

    Add files via upload

---
+++ operators in python.py
@@ -0,0 +1,30 @@
+##arethmatic operators
+a=10
+b=20
+print(a+b) #addition
+print(a-b) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a//b) #floor division (brings the no. to nearest whole number means removes the desimal after values)
+print(a%b) #modulusgives the remainder after division
+print(a**b) #exponentiation (a to the power of b)
+
+#comparison operators Gives the output in boolean values (true or false)
+print(a==b) #equal to
+print(a!=b) #not equal to
+print(a>b) #greater than
+print(a<b) #less than
+print(a>=b) #greater than or equal to
+print(a<=b) #less than or equal to
+
+#logical operators
+x=True
+y=True
+result=x and y #logical and (both conditions should be true)
+print(result)
+
+result=x or y #logical or (at least one condition should be true)
+print(result)
+
+result=not x #logical not (negates the value) inverts the value of x true becomes false and false becomes true
+print(result)
